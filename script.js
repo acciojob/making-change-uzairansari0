e205723b92016d118edc516cf38456e71c3190ab
Uzair <109786747+uzairansari0@users.noreply.github.com>
2026-01-20 10:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const makeChange = (c) => {
   // your name here
+	change = {
+		q:'',
+		d:'',
+		n:'',
+		p:'',
+	}
+
+	change.q = Math.floor(c / 25)
+	c%=25
+	
+	change.d = Math.floor(c / 10)
+	c%=10
+	
+	change.n = Math.floor(c / 5)
+	c%=5
+	
+
+	change.p = c
+
+	return change
 };
 
 // Do not the change the code below
+
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
